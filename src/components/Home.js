88999be284b5396513b11eacd660b29c823f90a4
bea_pascual <pascuals.beatriz@gmail.com>
2022-07-21 18:53:53 +0200

    Get Api info with fetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
-  
+  const [films, setfilms] = useState([]);
+
+  useEffect(()=> {
+    getApiInfo()
+  },[]) 
+
+  const getApiInfo = async () => {
+    let url = `https://api.themoviedb.org/3/movie/popular?api_key=fa97f60d1d3864cbc1194358693ca4b5&language=es-ES`;
+    const response = await fetch(url);
+    const res = await response.json();
+    // console.log(res.results)
+    setfilms(res.results)
+  }
+
+
+
   return (
     <div>Home</div>
   )
