ba9068ff492dbdc133ed97775e4dd743152c27ca
bea_pascual <pascuals.beatriz@gmail.com>
2022-07-22 13:14:53 +0200

    Add NavLink to a Header nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <section>
     <nav className="nav--container">
+    <NavLink to="title">
       <span className="nav--link">Title</span>
+    </NavLink>
+    <NavLink to="title_original">
       <span className="nav--link">V.O. Title</span>
+    </NavLink>
+    <NavLink to="overview">
       <span className="nav--link">Overview</span>
+    </NavLink>
     </nav>
 
     <form>
